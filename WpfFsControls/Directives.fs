343e891e5e08e84668e51c69e0688a0065c5ce2e
Ruxoz <github@just.sent.at>
2014-12-29 20:00:55 +0700

    Support RadioButton model binding.

--- WpfFsControls/Directives.fs
+++ WpfFsControls/Directives.fs
@@ -1,6 +1,9 @@
 ﻿namespace RZ.Wpf.Directives
 
+open System
 open System.Windows
+open System.Windows.Data
+open System.Windows.Controls
 
 type Evt() =
     static let setEvent (el: DependencyObject) (event: string) (property: string) =
@@ -18,4 +21,53 @@ type Evt() =
     static member SetToCommand(element: DependencyObject, value: string) =
         match value.Split([|"->"|], System.StringSplitOptions.None) with
         | [| event; property |] -> setEvent (element) (event.Trim()) (property.Trim())
-        | _                     -> failwithf "Invalid command format"
+        | _                     -> failwith "Invalid command format"
+
+[<AllowNullLiteral>]
+type ModelBinderInfo() =
+    member val Target = String.Empty with get, set
+    member val Value = String.Empty with get, set
+
+    member x.ChangeDC (fe: FrameworkElement) =
+        // specially for RadioButton
+        if fe.DataContext = null
+            then BindingOperations.ClearBinding(fe, RadioButton.IsCheckedProperty)
+            else let binding = Binding(x.Target, Source = fe.DataContext, Converter = x)
+                 in  fe.SetBinding(RadioButton.IsCheckedProperty, binding) |> ignore
+
+    member x.Rebound (fe: FrameworkElement) =
+        let expr = fe.GetBindingExpression RadioButton.IsCheckedProperty
+        in  expr.UpdateTarget()
+
+    interface IValueConverter with
+        member x.Convert(value, targetType, parameter, culture) =
+            value.ToString() = x.Value :> obj
+        member x.ConvertBack(value, targetType, parameter, culture) =
+            let is_checked = value :?> bool
+            in  if is_checked
+                then Enum.Parse(targetType, x.Value)
+                else Binding.DoNothing
+
+type Model() =
+    static let modelBinderProperty = DependencyProperty.RegisterAttached( "ModelBinder"
+                                                                        , typeof<ModelBinderInfo>
+                                                                        , typeof<Model> )
+    static let makeModelBinder (el: DependencyObject) =
+        match el.GetValue modelBinderProperty :?> ModelBinderInfo with
+        | null -> let m = ModelBinderInfo()
+                  in el.SetValue(modelBinderProperty, m)
+                  m
+        | model -> model
+
+    static member SetBind(el: DependencyObject, value: string) =
+        let fe = el :?> FrameworkElement
+        let model = makeModelBinder el
+        model.Target <- value
+        model.ChangeDC fe
+
+    static member SetValue(el: DependencyObject, value: string) =
+        let fe = el :?> FrameworkElement
+        let model = makeModelBinder el
+        model.Value <- value
+        model.Rebound fe
+
