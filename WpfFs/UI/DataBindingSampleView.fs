0083386057a76bd9d58dece62237f8345eb35057
Ruxoz <github@just.sent.at>
2014-12-29 22:28:12 +0700

    Finish SingleThread add/remove items in DataBindingSampleView.

---
+++ WpfFs/UI/DataBindingSampleView.fs
@@ -0,0 +1,29 @@
+﻿namespace WpfFs.UI
+
+open System.Windows
+open System.Windows.Controls
+
+open WpfFs.Models
+
+type DataBindingSampleView() as me =
+    inherit UserControl()
+    let mutable running_id = 1
+    let getId() = System.Threading.Interlocked.Increment(&running_id)
+
+    do  RZ.Wpf.XamlLoader.loadFromResource "DataBindingSample.xaml" (Some me) |> ignore
+    member private x.context with get() = me.DataContext :?> DatabindingSampleModel
+
+    member x.Add10Items  (_:obj, _:RoutedEventArgs) =
+        for i = 1 to 10 do
+            let id = getId()
+            in  x.context.Data.Add <| Person(Id = id, Name = "Test " + id.ToString())
+
+    member x.Remove5Items(_:obj, _:RoutedEventArgs) =
+        let removeTop() = if x.context.Data.Count > 0 then x.context.Data.RemoveAt 0
+        let rec remove = function
+                         | 0 -> ()
+                         | n -> removeTop()
+                                remove (n-1)
+        in  remove 5
+    
+    member x.ClearItems  (_:obj, _:RoutedEventArgs) = x.context.Data.Clear()
