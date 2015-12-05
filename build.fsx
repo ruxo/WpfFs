02a5d284b573365e224adec5fc0c22fb33e93cb8
Ruxo Zheng <rz@just.sent.at>
2015-12-05 10:04:56 +0700

    Finish simple build script.

--- build.fsx
+++ build.fsx
@@ -1,8 +1,24 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
 open Fake
 
+let buildDir = "./build/"
+
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "BuildApp" (fun _ ->
+    !! "./**/*.fsproj"
+    |> MSBuildRelease buildDir "Build"
+    |> Log "AppBuild-Output: "
+)
+
 Target "Default" (fun _ ->
     trace "Hello World from FAKE"
 )
+
+"Clean"
+    ==> "BuildApp"
+    ==> "Default"
 
 RunTargetOrDefault "Default"
