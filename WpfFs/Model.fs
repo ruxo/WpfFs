8539b7e32f27ceeb5a1a7667acf9a94ab3f0c79c
Ruxo Zheng <rz@just.sent.at>
2015-12-06 08:12:37 +0700

    Handle Selected from TreeViewItem.

--- WpfFs/Model.fs
+++ WpfFs/Model.fs
@@ -3,6 +3,20 @@
 open FSharp.Core.Printf
 open FSharp.ViewModule
 open System.Windows
+open RZ.Foundation;
+open System.Runtime.CompilerServices
+
+[<Extension>]
+[<AutoOpen>]
+type ObjEx =
+  [<Extension>] static member cast<'T>(o: obj) = o |> cast<'T>
+  [<Extension>] static member tryCast<'T>(o: obj) = o |> tryCast<'T>
+
+[<Extension>]
+[<AutoOpen>]
+type OptionEx =
+  [<Extension>] static member map(x, f) = x |> Option.map f
+  [<Extension>] static member get(x) = Option.get x
 
 type MainWindowEvents =
   | Invalid
@@ -11,7 +25,12 @@ type MainWindowEvents =
 type SelectConverter() =
   inherit FsXaml.EventArgsConverter<RoutedEventArgs,MainWindowEvents>(SelectConverter.TagCapture, Invalid)
 
-  static member private TagCapture routeArgs = SelectShow "hello"
+  static member private TagCapture routeArgs =
+    routeArgs
+      .Source
+      .tryCast<FrameworkElement>()
+      .map(fun fe -> SelectShow (fe.Tag.cast<string>()))
+      .get()
 
 type MainWindowModel() as me =
     inherit EventViewModelBase<MainWindowEvents>()
