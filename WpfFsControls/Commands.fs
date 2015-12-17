b1a75567741456cd98e85ade20c017a718f09d42
Ruxo Zheng <rz@just.sent.at>
2015-12-17 13:52:49 +0700

    Introduce new CommandParameter version that triggers ICommand.CanExecute()

--- WpfFsControls/Commands.fs
+++ WpfFsControls/Commands.fs
@@ -1,12 +1,14 @@
 ﻿namespace RZ.Wpf.Commands
 
 open System
+open System.Windows
 open System.Windows.Input
+open RZ.Foundation
 
 type RelayCommand(execute: Action<obj>, ?canExecute: Predicate<obj>) =
     let canExecuteChanged = new Event<EventHandler, EventArgs>()
     do
-        if execute = null then
+        if isNull execute then
             raise <| new ArgumentNullException("execute")
 
     interface ICommand with
@@ -21,3 +23,28 @@ type RelayCommand(execute: Action<obj>, ?canExecute: Predicate<obj>) =
     member this.UpdateCanExecuteCommand() = canExecuteChanged.Trigger(this, EventArgs.Empty)
 
     static member BindCommand fn = RelayCommand(new Action<obj>(fn)) :> ICommand
+
+module private CPB =
+  let updateCommandState (cmd: ICommand) =
+    match cmd with
+    | :? RoutedCommand -> CommandManager.InvalidateRequerySuggested()
+    | _ -> ()
+
+  let isCommandRequeriedOnChange (d: DependencyObject) (e: DependencyPropertyChangedEventArgs) =
+    let command = d |> tryCast<ICommand>
+    match box d with
+    | :? ICommandSource
+    | :? FrameworkElement
+    | :? FrameworkContentElement ->
+      ()
+    | _ -> ()
+
+
+type CommandParameterBehavior() =
+  static let commandChanged (d: DependencyObject) e = ()
+
+  static let IsCommandRequeriedOnChangeProperty =
+    DependencyProperty.RegisterAttached("IsCommandRequeriedOnChange",
+      typeof<bool>,
+      typeof<CommandParameterBehavior>,
+      UIPropertyMetadata(false, PropertyChangedCallback(commandChanged)))
