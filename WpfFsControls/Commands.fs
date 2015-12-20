9b422c2a433d44acc078c8a696db2f080f4bfbc3
Ruxo Zheng <rz@just.sent.at>
2015-12-20 12:52:56 +0700

    Create XamlFix module in Directives namespace, containing `CommandParameter` fix.

--- WpfFsControls/Commands.fs
+++ WpfFsControls/Commands.fs
@@ -1,9 +1,7 @@
 ﻿namespace RZ.Wpf.Commands
 
 open System
-open System.Windows
 open System.Windows.Input
-open RZ.Foundation
 
 type RelayCommand(execute: Action<obj>, ?canExecute: Predicate<obj>) =
     let canExecuteChanged = new Event<EventHandler, EventArgs>()
@@ -12,39 +10,14 @@ type RelayCommand(execute: Action<obj>, ?canExecute: Predicate<obj>) =
             raise <| new ArgumentNullException("execute")
 
     interface ICommand with
-        member this.CanExecute(param) = match canExecute with
+        member __.CanExecute(param) = match canExecute with
                                         | Some fn -> fn.Invoke(param)
                                         | None -> true
 
-        member this.Execute(param) = execute.Invoke(param)
+        member __.Execute(param) = execute.Invoke(param)
         [<CLIEvent>]
-        member this.CanExecuteChanged = canExecuteChanged.Publish
+        member __.CanExecuteChanged = canExecuteChanged.Publish
 
     member this.UpdateCanExecuteCommand() = canExecuteChanged.Trigger(this, EventArgs.Empty)
 
     static member BindCommand fn = RelayCommand(new Action<obj>(fn)) :> ICommand
-
-module private CPB =
-  let updateCommandState (cmd: ICommand) =
-    match cmd with
-    | :? RoutedCommand -> CommandManager.InvalidateRequerySuggested()
-    | _ -> ()
-
-  let isCommandRequeriedOnChange (d: DependencyObject) (e: DependencyPropertyChangedEventArgs) =
-    let command = d |> tryCast<ICommand>
-    match box d with
-    | :? ICommandSource
-    | :? FrameworkElement
-    | :? FrameworkContentElement ->
-      ()
-    | _ -> ()
-
-
-type CommandParameterBehavior() =
-  static let commandChanged (d: DependencyObject) e = ()
-
-  static let IsCommandRequeriedOnChangeProperty =
-    DependencyProperty.RegisterAttached("IsCommandRequeriedOnChange",
-      typeof<bool>,
-      typeof<CommandParameterBehavior>,
-      UIPropertyMetadata(false, PropertyChangedCallback(commandChanged)))
