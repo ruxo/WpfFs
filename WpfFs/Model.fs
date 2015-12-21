5a40569684bc86efe5e50929dbc3d4bba676a302
Ruxo Zheng <rz@just.sent.at>
2015-12-21 08:46:33 +0700

    Completely replace TreeView with Expanders.  Remove unused code.

--- WpfFs/Model.fs
+++ WpfFs/Model.fs
@@ -1,33 +1,17 @@
 ﻿namespace WpfFs.Models
 
-open FSharp.Core.Printf
 open FSharp.ViewModule
-open System.Windows
 open RZ.Foundation;
-open FSharp.Core.Fluent
-open System.Windows.Input
-open System
 
 type MainWindowEvents =
   | Invalid
   | SelectShow of string
-
-type SelectConverter() =
-  inherit FsXaml.EventArgsConverter<RoutedEventArgs,MainWindowEvents>(SelectConverter.TagCapture, Invalid)
-
-  static member private TagCapture routeArgs =
-    routeArgs.Source
-             .tryCast<FrameworkElement>()
-             .map(fun fe -> SelectShow (fe.Tag.cast<string>()))
-             |> Option.getOrElse (constant Invalid)
 
 type MainWindowModel() as me =
     inherit EventViewModelBase<MainWindowEvents>()
 
     let eventCommand = me.Factory.EventValueCommand()
     let xamlFileName = me.Factory.Backing(<@ me.XamlViewFilename @>, System.String.Empty)
-
-    let dummyEvent = new Event<EventHandler,EventArgs>()
 
     let helpCommand = me.Factory.CommandSync(fun _ -> System.Diagnostics.Process.Start "http://google.com" |> ignore)
 
@@ -42,10 +26,10 @@ type MainWindowModel() as me =
       |> Observable.subscribe handleEvents
       |> ignore
 
-    member x.XamlViewFilename with get() = xamlFileName.Value and set v = xamlFileName.Value <- v
+    member __.XamlViewFilename with get() = xamlFileName.Value and set v = xamlFileName.Value <- v
 
-    member x.Help: INotifyCommand = helpCommand
+    member __.Help: INotifyCommand = helpCommand
 
-    member x.EventCommand = eventCommand
-    member x.NavCommand = navCommand
+    member __.EventCommand = eventCommand
+    member __.NavCommand = navCommand
     
--- WpfFs/UI/Codebehind.fs
+++ WpfFs/UI/Codebehind.fs
@@ -13,36 +13,3 @@ type MainWindowController() =
     window.Root.CommandBindings.Add
       <| CommandBinding(ApplicationCommands.Help, fun _ _ -> startGoogle())
     |> ignore
-
-  (*
-type OldMainWindow() as me =
-    inherit System.Windows.Window()
-
-    do  RZ.Wpf.XamlLoader.loadFromResource "mainwindow.xaml" (Some me) |> ignore
-    let context = me.DataContext :?> MainWindowModel
-    let changeView (name:string) = context.OnUIEvent("CHANGEVIEW", name :> obj)
-
-    member this.ListBox_SelectionChanged(sender:obj, e:SelectionChangedEventArgs) =
-        if e.AddedItems.Count > 0 then
-            ignore <| MessageBox.Show( "You just selected " + e.AddedItems.[0].ToString())
-
-    member this.Button_Click(sender:obj, e:RoutedEventArgs) = ignore <| MessageBox.Show("You just clicked " + e.Source.ToString())
-
-    member this.AboutDialog_MouseRightButtonDown(sender: obj, e: MouseButtonEventArgs) =
-        this.Title <- "Source = " + e.Source.GetType().Name + ", Original Source = " + 
-                        e.OriginalSource.GetType().Name + " @ " + e.Timestamp.ToString()
-
-        let source = e.Source :?> Control
-
-        if source.BorderThickness <> Thickness(5.0) then
-            source.BorderThickness <- Thickness(5.0)
-            source.BorderBrush <- Brushes.Black
-        else
-            source.BorderThickness <- Thickness(0.0)
-
-    member private me.RunAbout (s:obj, e:RoutedEventArgs) = changeView "AboutDialog.xaml"
-    member private me.RunRoutedEventInAction (s:obj, e:RoutedEventArgs) = changeView "RoutedEventInActionFront.xaml"
-    member private me.RunDataBinding (s:obj, e:RoutedEventArgs) = changeView "DataBindingSample.xaml"
-    member private me.RunGridSharedSizeGroup (s:obj, e:RoutedEventArgs) = changeView "GridSharedSizeGroup.xaml"
-    member private me.ShowFlowDocument (s:obj, e:RoutedEventArgs) = changeView "FlowDocumentSample.xaml"
-    *)
