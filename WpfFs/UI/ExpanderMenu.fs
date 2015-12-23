d5515306b531ddc7600c815fd692d04ce859e515
Ruxo Zheng <rz@just.sent.at>
2015-12-23 09:31:17 +0700

    Start simple Expander user control.

---
+++ WpfFs/UI/ExpanderMenu.fs
@@ -0,0 +1,9 @@
+﻿namespace WpfFs.UI
+
+open System.Windows.Controls
+open RZ.Wpf.CodeBehind
+
+type ExpanderMenu() as me =
+  inherit UserControl()
+
+  do me.InitializeCodeBehind("ExpanderMenu.xaml")
