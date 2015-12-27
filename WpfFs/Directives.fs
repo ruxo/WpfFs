be6535dbe87b7e54ba1d922aa13236afa28325d5
Ruxo Zheng <rz@just.sent.at>
2015-12-27 09:12:21 +0700

    Correct RZ.Wpf references in WpfFs sample project.

--- WpfFs/Directives.fs
+++ WpfFs/Directives.fs
@@ -2,24 +2,30 @@
 
 open System
 open System.Windows
+open System.Diagnostics
+open FSharp.Core.Printf
+open RZ.Foundation
 
 type Ng() =
-    static let onMouseRightButtonDown (sender: obj) (e: Input.MouseButtonEventArgs) =
-        printfn "Source = %A, OriginalSource = %A @ %A" (e.Source.GetType().Name) (e.OriginalSource.GetType().Name) (e.Timestamp)
+    static let onMouseRightButtonDown _ (e: Input.MouseButtonEventArgs) =
+        kprintf Debug.Print "Source = %A, OriginalSource = %A @ %A" (e.Source.GetType().Name) (e.OriginalSource.GetType().Name) (e.Timestamp)
 
-        match e.Source with
-        | :? Controls.Control as source ->
+        e.Source
+          |> tryCast<Controls.Control>
+          |> Option.do' (fun source ->
                 if source.BorderThickness <> Thickness(5.0) then
                     source.BorderThickness <- Thickness(5.0)
                     source.BorderBrush <- Media.Brushes.Black
                 else
                     source.BorderThickness <- Thickness(0.0)
-        | _ -> ()
+             )
 
     static member SetBlockMark(el: DependencyObject, value: bool) =
-        match el with
-        | :? UIElement as ui -> if value then
-                                    ui.AddHandler(UIElement.MouseRightButtonDownEvent, Input.MouseButtonEventHandler(onMouseRightButtonDown), true)
-                                else
-                                    ui.RemoveHandler(UIElement.MouseRightButtonDownEvent, Input.MouseButtonEventHandler(onMouseRightButtonDown))
-        | _ -> ()
+        el
+          |> tryCast<UIElement>
+          |> Option.do' (fun ui ->
+               if value then
+                 ui.AddHandler(UIElement.MouseRightButtonDownEvent, Input.MouseButtonEventHandler(onMouseRightButtonDown), true)
+               else
+                 ui.RemoveHandler(UIElement.MouseRightButtonDownEvent, Input.MouseButtonEventHandler(onMouseRightButtonDown))
+             )
