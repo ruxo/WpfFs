2adcafd0ef7571f5b3a9ff4649c5c545e7f63b78
Ruxo Zheng <rz@just.sent.at>
2016-01-03 12:55:24 +0700

    Clean up WPF Loader code.
    Also split ResourceManager and Utils modules out of Wpf.fs file.

--- WpfFsControls/Controls.fs
+++ WpfFsControls/Controls.fs
@@ -12,35 +12,32 @@ type public Inclusion() as me =
     inherit System.Windows.Controls.Panel()
     let designMode = DesignerProperties.GetIsInDesignMode me
 
-    let loadUIElementFromResource filename =
-        System.Reflection.Assembly.GetEntryAssembly() |> XamlLoader.loadFromResource0 None filename
+    let loadUIElementFromResource resourceName =
+        System.Reflection.Assembly.GetEntryAssembly() |> XamlLoader.createFromResource resourceName
 
     let loadUiElement filename =
-        RZ.Wpf.XamlLoader.LoadWpfFromFile filename
+        XamlLoader.createFromFile filename
           |> Option.orTry (fun _ -> loadUIElementFromResource filename)
           |> Option.bind tryCast<UIElement>
 
-    let loadXamlRuntimeMode filename =
-        match loadUiElement filename with
-        | None -> []
-        | Some ui -> [ui]
+    let loadXamlRuntimeMode = loadUiElement >> Option.map List.singleton >> Option.getOrElse (constant [])
                    
     let loadXamlDesignMode filename =
         [Label(Content = filename) :> UIElement]
 
     let loadXamlTo = if designMode then loadXamlDesignMode else loadXamlRuntimeMode
 
-    static let OnXamlChanged (o: DependencyObject) (e: DependencyPropertyChangedEventArgs) =
+    static let onXamlChanged (o: DependencyObject) (e: DependencyPropertyChangedEventArgs) =
         match o with
         | :? Inclusion as this -> this.Children.Clear()
                                   let value = e.NewValue :?> string
                                   if not (String.IsNullOrEmpty value) then
                                       this.LoadXamlTo value
-                                      |> List.iter (fun ui -> ignore (this.Children.Add ui))
+                                      |> List.iter (this.Children.Add >> ignore)
         | _ -> failwith "Unexpected"
     
     static let xamlProperty = DependencyProperty.Register("Xaml", typeof<string>, typeof<Inclusion>,
-                                    FrameworkPropertyMetadata(PropertyChangedCallback(OnXamlChanged))
+                                    FrameworkPropertyMetadata(PropertyChangedCallback(onXamlChanged))
                               )
     static member XamlProperty with get() = xamlProperty
 
---
+++ WpfFsControls/ResourceManager.fs
@@ -0,0 +1,43 @@
+﻿namespace RZ.Wpf
+
+module ResourceManager =
+  open System
+  open System.Collections
+  open System.Reflection
+  open System.IO
+  open FSharp.Core.Fluent
+  open RZ.Foundation
+
+  let private getCandidateAssemblies() =
+    let primary = [ Assembly.GetCallingAssembly(); Assembly.GetEntryAssembly() ]
+    let isPrimary = (flip List.contains) primary
+    let isSystem (asm:Assembly) = asm.FullName.StartsWith("SYSTEM", StringComparison.OrdinalIgnoreCase)
+    let rest =
+       AppDomain
+        .CurrentDomain.GetAssemblies()
+        .filter(fun a -> not (isPrimary a || isSystem a))
+    primary |> Seq.append rest
+
+  let private getResourceLookup0 (asm: Assembly) =
+      let makeXamlString (stream: Stream) =
+          use reader = new StreamReader(stream)
+          in  reader.ReadToEnd()
+
+      let readResourceStream (stream: Stream) =
+          use reader = new System.Resources.ResourceReader(stream)
+          reader
+          |> Seq.cast :> seq<DictionaryEntry>
+          |> Seq.map (fun entry -> entry.Key :?> string, makeXamlString (entry.Value :?> Stream))
+          |> Map.ofSeq
+
+      let getWpfResourceName (asm: Assembly) = sprintf "%s.g.resources" <| asm.GetName().Name
+
+      let resourceName = getWpfResourceName asm
+      in  match asm.GetManifestResourceStream resourceName with
+          | null -> Map.empty
+          | stream -> readResourceStream stream
+
+  let getResourceLookup: Assembly -> Map<string,string> = Utils.memoize getResourceLookup0
+
+  let findWpfResource (name: string) = getResourceLookup >> Map.tryFind (name.ToLower())
+
