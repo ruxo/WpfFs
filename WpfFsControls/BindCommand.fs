b2928e3bef45f09369e845d5dc3ac4b76fe70333
Ruxo Zheng <rz@just.sent.at>
2016-01-04 13:13:33 +0700

    Start implementation of BindCommand.
    BindCommand should allow easy binding between event and ICommand without using Blend SDK.

---
+++ WpfFsControls/BindCommand.fs
@@ -0,0 +1,43 @@
+﻿namespace RZ.Wpf.Markups
+
+open System.Reflection
+open System.Windows
+open System.Windows.Markup
+open System.Windows.Input
+open System.Diagnostics
+open System
+open RZ.Foundation
+
+type private RoutedEventForwarder<'a when 'a :> RoutedEventArgs>(command: ICommand) =
+  member __.Invoke(sender:obj, e: 'a) = ()
+
+[<MarkupExtensionReturnType(typeof<MethodInfo>)>]
+type BindCommand(cmd: ICommand) =
+  inherit MarkupExtension()
+
+  let getDelegateTypeFromEvent(sp: IServiceProvider) =
+    sp.GetService(typeof<IProvideValueTarget>)
+      .cast<IProvideValueTarget>()
+    |> Option.ofObj
+    |> Option.bind (fun o -> o.TargetProperty.tryCast<EventInfo>())
+    |> Option.map (fun o -> o.EventHandlerType)
+
+  let createForward(delegateType: Type) = ()
+
+  let mutable command = cmd |> Option.ofObj
+
+  let fireCommand (sender:obj) (e:RoutedEventArgs) =
+    Debug.WriteLine("OK fire!")
+    e.Handled <- true
+
+  new() = BindCommand(null)
+
+  [<ConstructorArgument("command")>]
+  member __.Command with get() = command and set v = command <- v
+
+  override me.ProvideValue(serviceProvider) =
+    getDelegateTypeFromEvent(serviceProvider)
+      .do'(fun delType ->
+         Debug.WriteLine("Target type = {0}", delType))
+
+    RoutedEventHandler(fireCommand) |> box 
