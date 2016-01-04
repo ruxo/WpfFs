9adaffe957454f119328cd86a74ea6545224551d
Ruxo Zheng <rz@just.sent.at>
2016-01-04 16:37:07 +0700

    Create delegate wrapper to bind with a WPF event.

--- WpfFsControls/BindCommand.fs
+++ WpfFsControls/BindCommand.fs
@@ -6,38 +6,61 @@ open System.Windows.Markup
 open System.Windows.Input
 open System.Diagnostics
 open System
+open FSharp.Core.Fluent
 open RZ.Foundation
 
 type private RoutedEventForwarder<'a when 'a :> RoutedEventArgs>(command: ICommand) =
-  member __.Invoke(sender:obj, e: 'a) = ()
+  member __.Invoke(sender:obj, e: 'a) =
+    Debug.WriteLine("Hell yeah!!!!")
+    e.Handled <- true
+
+module private RoutedEventForwarder =
+  let routedEventForwarder = typedefof<RoutedEventForwarder<_>>
+
+  let of' = RZ.Wpf.Utils.memoize (fun routedEventType -> routedEventForwarder.MakeGenericType([| routedEventType |]))
 
 [<MarkupExtensionReturnType(typeof<MethodInfo>)>]
 type BindCommand(cmd: ICommand) =
   inherit MarkupExtension()
 
-  let getDelegateTypeFromEvent(sp: IServiceProvider) =
+  static let getDelegateTypeFromEvent(sp: IServiceProvider) =
     sp.GetService(typeof<IProvideValueTarget>)
       .cast<IProvideValueTarget>()
     |> Option.ofObj
     |> Option.bind (fun o -> o.TargetProperty.tryCast<EventInfo>())
     |> Option.map (fun o -> o.EventHandlerType)
 
-  let createForward(delegateType: Type) = ()
+  static let getArgType(delegateType: Type) =
+    match delegateType.GetMethod("Invoke") with
+    | null -> None
+    | m ->
+      let p = m.GetParameters()
+      if m.ReturnType = typeof<System.Void>
+         && p.Length = 2 
+         && p.[0].ParameterType = typeof<obj> 
+         && typeof<RoutedEventArgs>.IsAssignableFrom(p.[1].ParameterType) then
+        Some p.[1].ParameterType
+      else
+        Debug.WriteLine("Not a valid WPF event type!")
+        None
+
+  static let createWrapper (cmd: ICommand) delType argType =
+    let ft = argType |> RoutedEventForwarder.of'
+    let wrapper = Activator.CreateInstance(ft, [| box cmd |])
+    let m = ft.GetMethod("Invoke", BindingFlags.NonPublic|||BindingFlags.Instance)
+    Delegate.CreateDelegate(delType, wrapper, m)
+
+  static let createWrapper1 cmd delType = delType |> getArgType |> Option.map (createWrapper cmd delType)
 
   let mutable command = cmd |> Option.ofObj
-
-  let fireCommand (sender:obj) (e:RoutedEventArgs) =
-    Debug.WriteLine("OK fire!")
-    e.Handled <- true
 
   new() = BindCommand(null)
 
   [<ConstructorArgument("command")>]
   member __.Command with get() = command and set v = command <- v
 
-  override me.ProvideValue(serviceProvider) =
+  override __.ProvideValue(serviceProvider) =
     getDelegateTypeFromEvent(serviceProvider)
-      .do'(fun delType ->
-         Debug.WriteLine("Target type = {0}", delType))
-
-    RoutedEventHandler(fireCommand) |> box 
+      .bind(createWrapper1 cmd)
+      .map(box)
+      .get()
